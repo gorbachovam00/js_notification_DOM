ad5fea452ff09f46f4f0439951d8ecdb0b9e7893
gorbachovam00 <gorbachovamaria00@gmail.com>
2024-11-25 17:52:52 +0200

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,26 @@
+'use strict';
+const pushNotification = (posTop, posRight, title, description, type)=>{
+    const notification = document.createElement('div');
+    notification.className = `notification ${type}`;
+    notification.style.top = `${posTop}px`;
+    notification.style.right = `${posRight}px`;
+    const notificationTitle = document.createElement('h2');
+    notificationTitle.className = 'title';
+    notificationTitle.textContent = title;
+    notificationTitle.style.whiteSpace = 'nowrap';
+    notification.appendChild(notificationTitle);
+    const notificationDescription = document.createElement('p');
+    notificationDescription.textContent = description;
+    notification.appendChild(notificationDescription);
+    document.body.appendChild(notification);
+    setTimeout(()=>{
+        notification.style.display = 'none';
+    }, 2000);
+};
+document.addEventListener('DOMContentLoaded', ()=>{
+    pushNotification(10, 10, 'Title of Success message', "Message example.\n Notification should contain title and description.", 'success');
+    pushNotification(150, 10, 'Title of Error message', "Message example.\n Notification should contain title and description.", 'error');
+    pushNotification(290, 10, 'Title of Warning message', "Message example.\n Notification should contain title and description.", 'warning');
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
